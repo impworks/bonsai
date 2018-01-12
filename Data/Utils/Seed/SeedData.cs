f2dc01e5c5b590f28c553ca075ad7d8d28da16ae
Andrei Kurosh <mail@spark.ms>
2018-01-12 12:00:36 +0300

    Improved seed data.

--- Data/Utils/Seed/SeedData.cs
+++ Data/Utils/Seed/SeedData.cs
@@ -18,6 +18,7 @@ namespace Bonsai.Data.Utils.Seed
 
             var ctx = new SeedContext(db);
 
+            // parents
             var root = ctx.AddPage("Иван Иванов", true, "1990.01.01", descrSource: "SampleDescription.md", factsSource: "SampleFacts.json");
             var dad = ctx.AddPage("Петр Иванов", true, "1960.02.03", "2010.03.02");
             var mom = ctx.AddPage("Екатерина Иванова", false, "1965.03.04");
@@ -25,6 +26,26 @@ namespace Bonsai.Data.Utils.Seed
             ctx.AddRelation(root, RelationType.Parent, dad);
             ctx.AddRelation(mom, RelationType.Child, root);
             ctx.AddRelation(mom, RelationType.Spouse, dad, "1987.02.02-2010.03.02");
+
+            // wife 1
+            var w1 = ctx.AddPage("Анна Семенова", false, "1992.01.02");
+            var c1 = ctx.AddPage("Евгений Семенов", true, "2012.02.03");
+            var c2 = ctx.AddPage("Екатерина Семенова", false, "2013.04.01");
+            var w1f = ctx.AddPage("Николай Семенов", true, "1965.05.06");
+            var w1m = ctx.AddPage("Ирина Семенова", false, "1967.06.07");
+
+            ctx.AddRelation(root, RelationType.Spouse, w1, "2010.05.05");
+            ctx.AddRelation(root, RelationType.Child, c1);
+            ctx.AddRelation(root, RelationType.Child, c2);
+            ctx.AddRelation(w1, RelationType.Child, c1);
+            ctx.AddRelation(w1, RelationType.Child, c2);
+            ctx.AddRelation(w1, RelationType.Parent, w1f);
+            ctx.AddRelation(w1, RelationType.Parent, w1m);
+
+            // pet
+            var cat = ctx.AddPage("Барсик", true, "2014.05.06", type: PageType.Pet, descrSource: "Персидский кот");
+            ctx.AddRelation(cat, RelationType.Owner, root);
+            ctx.AddRelation(cat, RelationType.Owner, w1);
 
             db.SaveChanges();
         }
