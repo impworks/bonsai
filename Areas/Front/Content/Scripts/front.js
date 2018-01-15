4eb50e8f822747ab350fcad394f489a1c48216b7
Andrei Kurosh <mail@spark.ms>
2018-01-15 13:41:02 +0300

    Media dialog display improvements.

--- Areas/Front/Content/Scripts/front.js
+++ Areas/Front/Content/Scripts/front.js
@@ -1,15 +1,58 @@
 ﻿$(function () {
-    // media 
-    $('body').magnificPopup({
-        delegate: 'a.media-thumb-link',
+    // media
+    var prefix = 'media-';
+    var selector = 'a.media-thumb-link';
+
+    function updateHash(elem) {
+        if (elem == null) {
+            history.pushState(
+                "",
+                document.title,
+                window.location.pathname + window.location.search
+            );
+            return;
+        }
+
+        var id = elem.data('media');
+        window.location.hash = prefix + id;
+    }
+
+    var allLinks = $('body').magnificPopup({
+        delegate: selector,
         key: 'media-popup',
         type: 'ajax',
+        tClose: 'Закрыть',
+        tLoading: 'Загрузка...',
+        ajax: {
+            tError: 'Не удалось загрузить данные.'
+        },
         gallery: {
             enabled: true,
             preload: [0, 2],
             tPrev: 'Назад',
             tNext: 'Вперед',
             tCounter: '<span class="mfp-counter">%curr% из %total%</span>'
+        },
+        callbacks: {
+            open: function() {
+                updateHash(this.currItem.el);
+            },
+            close: function () {
+                updateHash(null);
+            },
+            change: function () {
+                updateHash(this.currItem.el);
+            },
         }
     });
+
+    var hash = window.location.hash;
+    if (hash !== null && hash.substr(1, prefix.length) === prefix) {
+        var media = hash.substr(prefix.length + 1);
+        var elem = $(selector + '[data-media="' + media + '"]');
+        var pos = allLinks.find(selector).index(elem);
+        if (pos !== -1) {
+            allLinks.magnificPopup('open', pos);
+        }
+    }
 });
