9ce5973fc43c92c029d8bad1e53bd464d1986d8a
Andrei Kurosh <mail@spark.ms>
2018-06-04 19:02:46 +0300

    Removed unused HomeVM.

--- Areas/Front/Controllers/HomeController.cs
+++ Areas/Front/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System.Threading.Tasks;
-using Bonsai.Areas.Front.Logic;
-using Bonsai.Areas.Front.ViewModels.Home;
-using Bonsai.Code.Infrastructure;
+﻿using Bonsai.Code.Infrastructure;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,35 +13,13 @@ namespace Bonsai.Areas.Front.Controllers
     [Authorize(Policy = AuthRequirement.Name)]
     public class HomeController : Controller
     {
-        public HomeController(PagePresenterService pages, MediaPresenterService media)
-        {
-            _pages = pages;
-            _media = media;
-        }
-
-        private readonly PagePresenterService _pages;
-        private readonly MediaPresenterService _media;
-
         /// <summary>
         /// Returns the main page.
-        /// It is currently empty.
         /// </summary>
         [Route("")]
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
-            var count = 5;
-            var lastPages = await _pages.GetLastUpdatedPagesAsync(count)
-                                        .ConfigureAwait(false);
-            var lastMedia = await _media.GetLastUploadedMediaAsync(count)
-                                        .ConfigureAwait(false);
-
-            var vm = new HomeVM
-            {
-                LastUpdatedPages = lastPages,
-                LastUploadedMedia = lastMedia
-            };
-
-            return View(vm);
+            return View();
         }
     }
 }
