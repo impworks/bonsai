49c95700d426f7d8beac584ab0ec11fb78b042ae
Andrei Kurosh <mail@spark.ms>
2018-06-09 14:13:48 +0300

    Popover in header.

---
+++ Areas/Front/Scripts/global-user-popover.js
@@ -0,0 +1,27 @@
+﻿$(function() {
+    var $toggle = $('.user-popover-toggle');
+    var $target = $('.user-image');
+    
+    $target.popover({
+        container: 'body',
+        content: $('#user-popover-contents').html(),
+        html: true,
+        offset: '0, 10',
+        placement: 'bottom',
+        trigger: 'manual',
+        template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-body user-popover-body"></div></div>'
+    });
+
+    $toggle.click(function () {
+        $target.popover('show');
+    });
+
+    $('body').on('click', function (e) {
+        function notIn($elem) {
+            return !$elem.is(e.target) && $elem.has(e.target).length === 0;
+        }
+        if (notIn($target) && notIn($toggle)) {
+            $target.popover('hide');
+        }
+    });
+})
