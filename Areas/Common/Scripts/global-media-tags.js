e73102ded7682001f466217b9cadd6fd33986076
Andrei Kurosh <mail@spark.ms>
2019-05-01 21:21:02 +0300

    Little fixes for #76.

--- Areas/Common/Scripts/global-media-tags.js
+++ Areas/Common/Scripts/global-media-tags.js
@@ -7,14 +7,6 @@
             .css('opacity', state ? 0.4 : 0);
     }
 
-    function highlightTag($tag) {
-        $('.media-wrapper .photo-tag')
-            .css('opacity', 0.2);
-
-        $tag.css('opacity', 0.6)
-            .tooltip('show');
-    }
-
     // display media wrapper links on image hover
     $b.on('mouseover', '.media-wrapper img.media-photo', function () {
         highlightAll(true);
@@ -25,13 +17,14 @@
     });
 
     $b.on('mouseover', '.media-wrapper .photo-tag', function () {
-        highlightTag($(this));
+        $('.media-wrapper .photo-tag').css('opacity', 0.2);
+        $(this).css('opacity', 0.6).tooltip('show');
     });
 
     $b.on('mouseover', '.media-tags .media-tag-link', function () {
         var id = $(this).data('tag-id');
         var $tag = $('.media-wrapper .photo-tag[data-tag-id="' + id + '"]');
-        highlightTag($tag);
+        $tag.css('opacity', 0.6).tooltip('show');
     });
 
     $b.on('mouseout', '.media-tags .media-tag-link', function () {
