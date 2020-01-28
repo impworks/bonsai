002b4ce441c07cc782056c6c7a7e6b46dcb8aeeb
Andrei Kurosh <mail@spark.ms>
2020-01-28 11:58:02 +0300

    Sources moved to /src/. Added SearchEngine unit test projects.

---
+++ src/Bonsai.Tests.Search/Fixtures/SearchCollection.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+
+namespace Bonsai.Tests.Search.Fixtures
+{
+    [CollectionDefinition("Search tests")]
+    public class SearchCollection: ICollectionFixture<SearchEngineFixture>
+    {
+    }
+}
