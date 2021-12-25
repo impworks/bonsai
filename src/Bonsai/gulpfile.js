733acd10068a62d928daa81ce2854b69c131eb6e
Andrei Kurosh <mail@spark.ms>
2021-12-25 12:33:02 +0300

    Buildfix: frontend packages updated.

--- src/Bonsai/gulpfile.js
+++ src/Bonsai/gulpfile.js
@@ -1,7 +1,7 @@
 /// <binding ProjectOpened='watch, dev' />
 const gulp = require('gulp');
 
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const mincss = require('gulp-clean-css');
 
 const concat = require('gulp-concat');
