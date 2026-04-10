4cf3d09ee8719919fa1e9433dc0b882edfd5ab83
Andrei Kurosh <mail@spark.ms>
2026-04-10 19:01:07 +0200

    Mobile-friendly UI: better page view.

--- src/Bonsai/Areas/Common/Scripts/global-mobile-sidebar.js
+++ src/Bonsai/Areas/Common/Scripts/global-mobile-sidebar.js
@@ -1,8 +1,10 @@
 $(function () {
     $('.sidebar-toggle').on('click', function () {
+        var $collapsed = $(this).siblings('.sidebar-collapsed');
         var $details = $(this).siblings('.sidebar-details');
         var $icon = $(this).find('.fa');
         var isOpen = $details.hasClass('open');
+        $collapsed.toggleClass('d-none', !isOpen);
         $details.toggleClass('open');
         $icon.toggleClass('fa-chevron-down', isOpen).toggleClass('fa-chevron-up', !isOpen);
     });
