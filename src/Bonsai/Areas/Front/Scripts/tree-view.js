faeca3da20eff0863b513434c774d691c9dae06f
Andrei Kurosh <mail@spark.ms>
2026-04-22 22:10:09 +0200

    Further improvements for the mobile theme: search view, tree display.

--- src/Bonsai/Areas/Front/Scripts/tree-view.js
+++ src/Bonsai/Areas/Front/Scripts/tree-view.js
@@ -4,8 +4,9 @@ $(function () {
     var $view = $('.tree-view'),
         $iframe = $view.find('iframe'),
         $toggles = $view.find('.cmd-switch-tree'),
+        $kindLabel = $view.find('.cmd-tree-kind-label'),
         $newWin = $view.find('.cmd-new-window');
-    
+
     if($view.length === 0)
         return;
 
@@ -17,20 +18,24 @@ $(function () {
         e.preventDefault();
 
         var $this = $(this);
+        var kind = $this.data('kind');
+        var label = $this.data('label') || $this.text().trim();
 
         $toggles.removeClass('active');
-        $this.addClass('active');
+        $toggles.filter('[data-kind="' + kind + '"]').addClass('active');
 
         var url = $this.attr('href');
         $newWin.attr('href', url);
         $iframe.attr('src', url);
+        $kindLabel.text(label);
 
-        window.localStorage.setItem(key, $this.data('kind'));
+        window.localStorage.setItem(key, kind);
     });
 
     var state = window.localStorage.getItem(key);
     if (state) {
         $toggles.filter('[data-kind="' + state + '"]')
+                .first()
                 .trigger('click');
     }
 });
